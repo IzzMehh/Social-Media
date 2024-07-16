2760f434532a58c8e14b7fa183a49ee8ee5edbfb
Gaurav <gauravbishtyt@gmail.com>
2024-07-16 14:14:33 +0530

    Added toasts ui for login/signup errors

--- VeWen/src/pages/Login.jsx
+++ VeWen/src/pages/Login.jsx
@@ -4,7 +4,8 @@ import {Link, useNavigate} from "react-router-dom"
 import authservice from '../appwrite/Auth'
 import {useDispatch} from "react-redux"
 import { login} from '../store/authSlice'
-import {Loader} from '../components/index'
+import {Loader, Uploading} from '../components/index'
+import { Success,Error } from '../components/ui/Toasts/index'
 
 function Login() {
   const {register,handleSubmit,formState:{errors}} = useForm()
@@ -14,6 +15,7 @@ function Login() {
 
   const [isLogged,setLogged] = React.useState(true)
   const [error,setError] = React.useState(false)
+  const [loading,setloading] = React.useState(false)
 
   React.useEffect(()=>{
     authservice.getCurrentUser().then(data=>{
@@ -26,6 +28,7 @@ function Login() {
   },[])
 
   const con = async(data) =>{
+    setloading(true)
       const session = await authservice.login(data)
       if(session){
         const userData = await authservice.getCurrentUser()
@@ -33,15 +36,17 @@ function Login() {
           dispatch(login(userData))
           navigate('/home')
         }
+        setloading(false)
       }else{
         setError(true)
+        setloading(false)
       }
   }
   
   return (
     isLogged ? <Loader/> :     <div className='w-full flex'>
     <div className='text-white flex justify-center w-full lg:w-[60%] '>
-      <div className='mt-[50px]  flex  justify-center w-[600px]'>
+      <div className='mt-[50px] flex  justify-center w-[600px]'>
         <div className='w-[90%]'>
       <div className='text-4xl font-title font-bold text-[#474BCA] mb-5'>VeWen</div>
       <div className='text-4xl font-content font-bold mb-2'>Login now</div>
@@ -53,7 +58,7 @@ function Login() {
         <input {...register('email',{required:"Please Enter the Email",pattern:{
           value:/^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
           message:'Invalid Email',
-        }})} placeholder='Enter your email id' className='px-3 py-2 outline-none bg-transparent border rounded-lg w-[90%]' type="email" name="email" />
+        }})} placeholder='Enter your email id' className='px-3 py-2 outline-none bg-transparent border rounded-lg w-[90%]' type="email" name="email" autoComplete='on'/>
         </div>
         {errors.email && <p className='text-red-600'>{errors.email.message}</p> }
         
@@ -66,12 +71,12 @@ function Login() {
           message:"Password must be at least 8 characters",
         },
       }
-        )} placeholder='Enter your password' className='px-3 py-2 outline-none bg-transparent border rounded-lg w-[90%]' type="password" name="password" />
+        )} placeholder='Enter your password' className='px-3 py-2 outline-none bg-transparent border rounded-lg w-[90%]' type="password" name="password"  autoComplete='on'/>
         </div>
         {errors.password && <p className='text-red-600'>{errors.password.message}</p> }
-        {error ? <div className='text-red-600'>Invalid credentials. Please check the email and password.</div> : null}
+        {error ?  <Error message={"Invalid credentials. Please check the email and password."} setError={setError}/> : null}
 
-        <button className='bg-blue-700 w-[90%] py-3 rounded-md mt-3'>Login</button>
+        <button className='bg-blue-700 w-[90%] py-3 rounded-md mt-3'>{loading ? <Uploading/> : 'Login'}</button>
         <div className=' text-sm mt-2 sm:text-base'>Not registered yet? <span className='text-blue-600'>Create an account</span> <Link to="/signup" className='text-[#FFA3BE]'>SignUp</Link></div>
         
       </form>
