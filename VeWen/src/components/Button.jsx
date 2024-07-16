1048198c8fe6e7ef48ffbb72192fddde58cf39d7
Gaurav <gauravbishtyt@gmail.com>
2024-07-16 18:58:57 +0530

    remove button outline

--- VeWen/src/components/Button.jsx
+++ VeWen/src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Button({text,classes,svg=null,sideBarButton=false}) {
     return (
         <button
-            className={`"bg-[#292929] border-2 ${sideBarButton ? 'border-none' :'border-[#3e3e3e]'} rounded-lg text-white px-6 py-3 hover:border-[#fff] cursor-pointer transition" ${classes}`}
+            className={`"bg-[#292929] outline-none border-2 ${sideBarButton ? 'border-none' :'border-[#3e3e3e]'} rounded-lg text-white px-6 py-3 hover:border-[#fff] cursor-pointer transition" ${classes}`}
         >
             <div className='flex items-center'>
             {svg ? <ion-icon name={svg}></ion-icon> : null}
